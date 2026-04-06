64bc21f35577af51e91508cac1c4923d72da77b0
adekunlebamz <adekunlebamz@users.noreply.github.com>
2026-04-06 19:04:52 +0100

    chore: export useInput in hooks barrel

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,6 +7,7 @@ export * from './useAsync';
 export * from './useClickOutside';
 export * from './useCopyToClipboard';
 export * from './useDocumentTitle';
+export * from './useInput';
 export * from './useInterval';
 export * from './useKeyboardEvent';
 export * from './useLocalStorage';
