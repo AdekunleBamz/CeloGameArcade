b449dd5095e339fbc3c8daadd5e53855c8c22877
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 07:56:58 +0100

    feat: export CELO_EXPLORER_URL, ALFAJORES_EXPLORER_URL, DEFAULT_CELO_RPC_URL, getExplorerTxUrl, getExplorerAddressUrl from SDK

--- packages/celo-arcade-sdk/src/index.ts
+++ packages/celo-arcade-sdk/src/index.ts
@@ -1,12 +1,17 @@
 export type { Address, ArcadeSdkConfig, StableTokenMetadata, PlayerStats, SdkLeaderboardEntry, TransactionStatus, DifficultyKey, ArcadeChainId } from './types';
 export type { ContractAbi } from './abi';
 export {
+  ALFAJORES_EXPLORER_URL,
   BASIS_POINTS,
   CELO_ALFAJORES_CHAIN_ID,
+  CELO_EXPLORER_URL,
   CELO_MAINNET_CHAIN_ID,
+  CLAIM_COOLDOWN_DAYS,
   CLAIM_COOLDOWN_SECONDS,
   CREATOR_SHARE_BPS,
+  DEFAULT_CELO_RPC_URL,
   DEFAULT_CONTRACT_ADDRESS,
+  DEFAULT_ENTRY_FEE_STRING,
   DEFAULT_MINIPAY_FEE_CURRENCY,
   DEFAULT_STABLE_TOKEN,
   DEFAULT_STABLE_TOKEN_ADDRESS,
@@ -20,10 +25,13 @@ export {
   GameType,
   LEADERBOARD_SIZE,
   MAX_BASIS_POINTS,
+  MAX_LEADERBOARD_SIZE,
   MAX_SCORE,
   MIN_SCORE,
   PRIZE_SHARE_BPS,
   SUPPORTED_CHAIN_IDS,
+  getExplorerAddressUrl,
+  getExplorerTxUrl,
 } from './constants';
 export { createArcadeConfig, DEFAULT_ARCADE_CONFIG, getDefaultEntryFee, resolveStableTokenSymbol, isArcadeConfig } from './config';
 export { ContractEvent } from './events';
