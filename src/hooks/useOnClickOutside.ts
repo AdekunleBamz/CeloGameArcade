dbe64c5e77b4b730b0c13d30ca3c3820932656e3
AdekunleBamz <bams.kunle@gmail.com>
2026-04-24 21:11:16 +0100

    Add useOnClickOutside hook for modal dismissal

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,23 @@
+'use client'
+
+import { useEffect, RefObject } from 'react'
+
+/**
+ * Triggers callback when clicking outside a referenced element.
+ * @param ref - Element reference.
+ * @param handler - Callback to invoke.
+ */
+export function useOnClickOutside(ref: RefObject<HTMLElement>, handler: () => void) {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) return
+      handler()
+    }
+    document.addEventListener('mousedown', listener)
+    document.addEventListener('touchstart', listener)
+    return () => {
+      document.removeEventListener('mousedown', listener)
+      document.removeEventListener('touchstart', listener)
+    }
+  }, [ref, handler])
+}
