8ca51ac8d244b653ffc695cfb70a9a4e968a0fca
AdekunleBamz <bams.kunle@gmail.com>
2026-04-24 21:01:43 +0100

    Add iOS standalone web app meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <meta
           name="talentapp:project_verification"
