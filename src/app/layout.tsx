9645431aa5e3face7b67b8938dc67d320a242618
AdekunleBamz <bams.kunle@gmail.com>
2026-04-24 21:01:44 +0100

    Add Android web app capable meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,6 +79,8 @@ export default function RootLayout({
         
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <meta
           name="talentapp:project_verification"
