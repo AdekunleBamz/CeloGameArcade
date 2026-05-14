d6f71b0fd8f2bef2e066004c7e557a5cfd28417a
AdekunleBamz <bams.kunle@gmail.com>
2026-05-14 20:47:54 +0100

    docs(hooks): add returns annotation to useDocumentTitle

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 /**
  * Updates the document title dynamically.
  * @param title - New page title.
+ * @returns void
  */
 export function useDocumentTitle(title: string) {
   useEffect(() => {
